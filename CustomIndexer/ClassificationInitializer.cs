c3ab99ab55fc5800018c60b6747ada65807b18df
Eugeni96 <e.a.tseitlin@gmail.com>
2015-11-03 00:10:19 +0300

    add classification init and update engine

---
+++ CustomIndexer/ClassificationInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adam.Core;
+using Adam.Core.Classifications;
+
+namespace CustomIndexer
+{
+    public static class ClassificationInitializer
+    {
+        public static void InitializeSpecifications(Application app)
+        {
+            Classification classification = new Classification(app);
+            if (classification.TryLoad(new ClassificationPath("/Cubus")) == TryLoadResult.NotFound)
+            {
+                classification.AddNew();
+                classification.Name = "Cubus";
+                classification.Save();
+
+                Classification video = new Classification(app);
+                video.AddNew(classification.Id);
+                video.Name = "Video";
+                
+                Classification audio = new Classification(app);
+                audio.AddNew(classification.Id);
+                audio.Name = "Audio";
+                
+                audio.Save();
+                video.Save();
+            }
+        }
+    }
+}
--- CustomIndexer/CoubusIndexerEngine.cs
+++ CustomIndexer/CoubusIndexerEngine.cs
@@ -3,10 +3,13 @@ using Adam.Core.Fields;
 using Adam.Core.Indexer;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using Adam.Core.Classifications;
 
 namespace CustomIndexer
 {
@@ -17,10 +20,36 @@ namespace CustomIndexer
         {
         }
 
+
         protected override void OnCatalog(CatalogEventArgs e)
         {
             base.OnCatalog(e);
             // Добавить классификации в зависимости от расширения
+
+            // пока что дикий хардкод с путями и расширениями
+            List<string> videoExtensions = new List<string>()
+            {
+                ".mp4",
+                ".avi",
+                ".mkv"
+            };
+            List<string> audioExtensions = new List<string>()
+            {
+                ".flac",
+                ".mp3",
+                ".wav"
+            };
+            
+            if (videoExtensions.Any(x => x.Equals(Path.GetExtension(e.Path))))
+            {
+                e.Record.Classifications.Add(new ClassificationPath("/Cubus/Video"));
+            }
+            if (audioExtensions.Any(x => x.Equals(Path.GetExtension(e.Path))))
+            {
+                e.Record.Classifications.Add(new ClassificationPath("/Cubus/Audio"));
+            }
+
+
             String metaFilePath = System.IO.Path.ChangeExtension(e.Path, ".cm");
             if (!System.IO.File.Exists(metaFilePath))
                 return;
