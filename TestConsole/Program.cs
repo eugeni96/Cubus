7d1720b12c5a8f8a8a46555afe5636f3caf6996a
IgorDemidov <igr.demidov@gmail.com>
2015-11-04 21:48:39 +0300

    +++

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Adam.Core;
+using Adam.Core.Classifications;
+using Adam.Core.Management;
+using Adam.Core.Orders;
+using Adam.Core.Records;
+using Adam.Core.Search;
 
 namespace TestConsole
 {
@@ -10,7 +16,22 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            Application app = new Application();
+            LogOnStatus status = app.LogOn("TRAINING", "Demidov", "123456");
+            Console.WriteLine(status);
+            if (status != LogOnStatus.LoggedOn)
+            {
+                throw new UnauthorizedAccessException();
+            }
 
+            Record rec = new Record(app);
+            Guid recId;
+            Guid.TryParse("46e678e9-24af-4364-8926-a54600dcf188", out recId);
+            rec.Load(recId);
+            string s = rec.Files.LatestMaster.Path;
+
+            Console.WriteLine(s);
+            Console.ReadLine();
         }
     }
 }
